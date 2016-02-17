d1eb6b22fc533ecb1e93ac045eeb4f536f46875c
kyonghee kim <khkim@woowahan.com>
2016-02-17 18:26:42 +0900

    고객조회 테스트
    jsonPath  유효성 체크 추가

--- src/test/java/com/woowahan/demo/controller/CustomerControllerTest.java
+++ src/test/java/com/woowahan/demo/controller/CustomerControllerTest.java
@@ -1,5 +1,15 @@
 package com.woowahan.demo.controller;
 
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static
+        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 import com.woowahan.SpringBootDemoApplication;
 import com.woowahan.demo.domain.Customer;
 import com.woowahan.demo.service.CustomerService;
@@ -13,19 +23,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.Charset;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 
 /**
  * Created by jhhan on 2016-02-12.
  * 업주관련 컨트롤러 클래스
+ * TODO : 테스트를 위한 기본 목업 동작 확인
  * TODO : 업주조회 API
  * TODO : 업주목록 조회 API
  * TODO : 업주생성 API
@@ -59,22 +67,41 @@ public class CustomerControllerTest {
     }
 
     /**
-     * Todo 리팩토링 필요
-     * @throws Exception
+     * 기본 목업 동작이 잘 되는지 체크
      */
     @Test
-    public void testCustomerReadById_Customers의_Value_를_체크한다() throws Exception {
+    public void test_목업기본동작체크() throws Exception {
+        mockMvc.perform(get("/customers/" ))
+                .andExpect( status().isNotFound() );
+    }
+
+    /**
+     * TODO : 고객조회를 위한 테스트 고객등록
+     * TODO : 등록된 고객 id로 조회 목업작성
+     * TODO : 리턴된 json과 값을 비교
+     */
+    @Test
+    public void test_아이디로_고객_조회() throws Exception {
         Customer customer = new Customer(null, "한", "재현");
         Customer createdCustomer = this.customerService.create(customer);
 
         Long expectedId = createdCustomer.getId();
 
-        mockMvc.perform( get("/customers/"+expectedId) )
-                .andExpect( status().isOk() )
-                .andExpect( content().contentType(mediaType) )
-                .andExpect( content().string(containsString("재현")) );
+        mockMvc.perform( get("/customers/" + expectedId) )
+                .andExpect(status().isOk() )
+                .andExpect(content().contentType(mediaType) )
+                .andExpect(
+                        jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(
+                        jsonPath("$.lastName").value(customer.getLastName()))
+                .andExpect(jsonPath("$.*",hasSize(3)))
+                .andExpect(jsonPath("$.lastName",is(customer.getLastName())))
+                .andExpect(jsonPath("$.lastName",is(any(String.class))))
+                .andDo(MockMvcResultHandlers.print());
 
     }
+
+
 
 
 }
